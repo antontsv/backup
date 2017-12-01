851b5c757a138ea4f82ffd888217c8e08618b009
Anton Tsviatkou <anton.tsviatkou@oracle.com>
2017-11-30 23:57:46 -0800

    Add test for output label text

--- main_test.go
+++ main_test.go
@@ -1,6 +1,11 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/fatih/color"
+)
 
 func TestDestParsing(t *testing.T) {
 	type TestCase struct {
@@ -26,6 +31,27 @@ func TestDestParsing(t *testing.T) {
 			}
 			if path != tc.expPath {
 				t.Errorf("Expected to parse '%s' as path, but got: '%s'", tc.expPath, path)
+			}
+		})
+	}
+}
+
+func TestLabels(t *testing.T) {
+	color.NoColor = true
+	type testCase struct {
+		name string
+		f    func() string
+	}
+
+	tests := []testCase{
+		{"Google", googlePrint},
+		{"Amazon", amazonPrint},
+	}
+
+	for _, tc := range tests {
+		t.Run(fmt.Sprintf("Label for %s", tc.name), func(t *testing.T) {
+			if tc.f() != tc.name {
+				t.Errorf("expected '%s', got '%s'", tc.name, tc.f())
 			}
 		})
 	}
